0d6fd1c76ca2ad8ea5c4ff36a3195c2fec1a124a
Jose Correa <jd_correa1@hotmail.com>
2018-06-18 09:00:28 -0500

    Reproduccion de Sonidos

---
+++ src/pages/home/home.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ANIMALES } from "../../data/data.animales";
+import { Animal } from "../../interfaces/animal.interface";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  animales:Animal[] = [];
+
+  constructor() {
+    this.animales = ANIMALES.slice(0);
+  }
+
+  reproducir(animal:Animal){
+    console.log(animal);
+
+    let audio = new Audio();
+    audio.src = animal.audio;
+
+    audio.load();
+    audio.play();
+
+    animal.reproduciendo = true;
+
+    setTimeout( () => animal.reproduciendo = false, animal.duracion * 1000);
+  }
+
+}
